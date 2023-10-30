1d24b01aa4216b77b895dfe82bcf2ec1932e351f
Belyaev.AK <belyaev.ak@tversu.ru>
2023-10-30 16:11:42 +0300

    Temporarly disable dygraph in plotFixedOrigin

--- R/plotFixedOrigin.R
+++ R/plotFixedOrigin.R
@@ -47,7 +47,7 @@
 #'
 #' @export
 
-plotFixedOrigin <- function(ts, fc = NULL, series, origin, method = NULL, graphLib = "dygraph") {
+plotFixedOrigin <- function(ts, fc = NULL, series, origin, method = NULL, graphLib = "ggplot") {
   # Error handling
   # For TSTS schema
   if (!is.data.frame(ts)){
@@ -62,56 +62,56 @@ plotFixedOrigin <- function(ts, fc = NULL, series, origin, method = NULL, graphL
     stop("The column timestamp_dbo of TSTS schema requires an appropriate time-based object")
   }
 
-  if (graphLib == "dygraph"){
-  # dplyr::filter data
-    M2 <- dplyr::filter(ts, series_id == series)
-    xts1 <- xts::xts(M2$value, order.by=M2$timestamp_dbo)
-    names(xts1) <- "TS"
+  # if (graphLib == "dygraph"){
+  # # dplyr::filter data
+  #   M2 <- dplyr::filter(ts, series_id == series)
+  #   xts1 <- xts::xts(M2$value, order.by=M2$timestamp_dbo)
+  #   names(xts1) <- "TS"
 
-    if (is.null(fc)) {
-      p <- dygraphs::dygraph( xts1, main = paste("Graph of", series), xlab = "Time") %>%
-           dygraphs::dyRangeSelector(height = 20) %>%
-            dygraphs::dyOptions(drawPoints = TRUE, pointSize = 2)
-    } else {
-    # For FTS schema
-      if (!is.data.frame(fc)){
-        stop("Argument fc should be a data frame.")
-      }
-      if (!sum(is.element(c("series_id", "timestamp_dbo", "method_id", "forecast"), colnames(fc))) == 4) {
-        stop("Check the column names of input data frame fc. The input data ts needed in the form of
-           a data frame containing columns named  'series_id', forecast, method_id, and 'timestamp_dbo'.")
-     }
-     if (!xts::is.timeBased(fc$timestamp_dbo)) {
-       stop("The column timestamp_dbo of FTS shema requires an appropriate time-based object")
-     }
-     M <- dplyr::filter(fc, series_id == series & origin_timestamp == origin)
+  #   if (is.null(fc)) {
+  #     p <- dygraphs::dygraph( xts1, main = paste("Graph of", series), xlab = "Time") %>%
+  #          dygraphs::dyRangeSelector(height = 20) %>%
+  #           dygraphs::dyOptions(drawPoints = TRUE, pointSize = 2)
+  #   } else {
+  #   # For FTS schema
+  #     if (!is.data.frame(fc)){
+  #       stop("Argument fc should be a data frame.")
+  #     }
+  #     if (!sum(is.element(c("series_id", "timestamp_dbo", "method_id", "forecast"), colnames(fc))) == 4) {
+  #       stop("Check the column names of input data frame fc. The input data ts needed in the form of
+  #          a data frame containing columns named  'series_id', forecast, method_id, and 'timestamp_dbo'.")
+  #    }
+  #    if (!xts::is.timeBased(fc$timestamp_dbo)) {
+  #      stop("The column timestamp_dbo of FTS shema requires an appropriate time-based object")
+  #    }
+  #    M <- dplyr::filter(fc, series_id == series & origin_timestamp == origin)
 
-     time <- M[1:length(unique(M$horizon)),]$timestamp_dbo
-     out <-matrix(NA, nrow = length(unique(M$horizon)), ncol = length(unique(M$method_id)))
-     df = data.frame(out)
-     colnames(df) <- unique(M$method_id)
+  #    time <- M[1:length(unique(M$horizon)),]$timestamp_dbo
+  #    out <-matrix(NA, nrow = length(unique(M$horizon)), ncol = length(unique(M$method_id)))
+  #    df = data.frame(out)
+  #    colnames(df) <- unique(M$method_id)
 
-     for(i in as.vector(unique(M$method_id))){
-       df[, i] <- dplyr::filter(M, method_id == i)$forecast
-     }
+  #    for(i in as.vector(unique(M$method_id))){
+  #      df[, i] <- dplyr::filter(M, method_id == i)$forecast
+  #    }
 
-     df2 <- cbind(df[method[1:length(method)]])
-     xts2 <- xts::xts(df2, order.by=time)
-     out <- cbind(xts1, xts2)
-     p <- dygraphs::dygraph(out, main = series, xlab = "Time") %>%
-          dygraphs::dyRangeSelector(height = 20) %>%
-          dygraphs::dyOptions(drawPoints = TRUE, pointSize = 2) %>%
-          dygraphs::dyLegend(width = 300)
-     }
-     a <- length(time(xts1)) - length(time(xts2))
-     p <- p %>% dygraphs::dyEvent(time(xts1)[a], "Forecast origin", labelLoc = "bottom", strokePattern = "dotted")
-     p <- p %>% dygraphs::dyShading(from = time(xts1)[a], to = time(xts1)[length(time(xts1))], color = "#F5F5F5")
-  # p <- p %>% dygraphs::dyOptions(includeZero = TRUE,
-  #                      axisLineColor = "navy",
-  #                      gridLineColor = "lightblue")
+  #    df2 <- cbind(df[method[1:length(method)]])
+  #    xts2 <- xts::xts(df2, order.by=time)
+  #    out <- cbind(xts1, xts2)
+  #    p <- dygraphs::dygraph(out, main = series, xlab = "Time") %>%
+  #         dygraphs::dyRangeSelector(height = 20) %>%
+  #         dygraphs::dyOptions(drawPoints = TRUE, pointSize = 2) %>%
+  #         dygraphs::dyLegend(width = 300)
+  #    }
+  #    a <- length(time(xts1)) - length(time(xts2))
+  #    p <- p %>% dygraphs::dyEvent(time(xts1)[a], "Forecast origin", labelLoc = "bottom", strokePattern = "dotted")
+  #    p <- p %>% dygraphs::dyShading(from = time(xts1)[a], to = time(xts1)[length(time(xts1))], color = "#F5F5F5")
+  # # p <- p %>% dygraphs::dyOptions(includeZero = TRUE,
+  # #                      axisLineColor = "navy",
+  # #                      gridLineColor = "lightblue")
 
-    p
-   }
+  #   p
+  #  }
 
   if (graphLib == "ggplot"){
     ts <- dplyr::filter(ts, series_id == series)
